8a0a736c440dfa706f212ad837fe5efe9b344a29
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-06-17 16:36:11 -0700

    File Structure
    Added the base layout for file structure.

---
+++ src/test/formatting.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+print("Number of Dimensions is: ", arr.ndim)
+print("Shape of array is: ", arr.shape)
+print("Size of array is: ", arr.size)
+print("Array stores elements of type: ", arr.dtype)
