b5e363025ac5da19e6dfa0a90e65477444c34b11
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-10-02 18:49:03 -0700

    Updated plot for trajectory plotting as well + Ramsete Test CSV

--- src/robotics/plotOverTime.py
+++ src/robotics/plotOverTime.py
@@ -4,17 +4,18 @@ import csv
 import numpy as np
 
 
-f = open('test0.csv')
+f = open('test1.csv')
 csv_f = list(csv.reader(f))
 
 plt.ion()
 
 fig, ax = plt.subplots()
-x, y = [float(csv_f[0][1])], [-1 * float(csv_f[0][2])]
+x, y = [float(csv_f[0][1])], [float(csv_f[0][2])]
+xd, yd = [float(csv_f[0][4])], [float(csv_f[0][5])]
 sc = ax.scatter(x, y)
 plt.xlim(0, 80)
 plt.ylim(0, 80)
-plt.title("Nonlinear Pose Estimation")
+plt.title("Nonlinear Pose Estimation (Blue) vs Trajectory(Orange)")
 plt.xlabel("x (inches)")
 plt.ylabel("y (inches)")
 plt.draw()
@@ -27,7 +28,12 @@ for row in csv_f:
     plt.pause(timeToSleep)
 
     x.append(float(csv_f[i][1]))
-    y.append(-1 * float(csv_f[i][2]))
+    y.append(float(csv_f[i][2]))
 
-    sc.set_offsets(np.c_[x, y])
+    xd.append(float(csv_f[i][4]))
+    yd.append(float(csv_f[i][5]))
+
+    plt.scatter(x, y, color='orange')
+    plt.scatter(xd, yd, color='blue')
+
     fig.canvas.draw_idle()
