0a1fda973d6f818bd3b16cd1e89d1a3cdfb3d17e
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-10-22 09:12:52 -0700

    Hour Logger + Pose estimation graphs

--- src/robotics/plotOverTime.py
+++ src/robotics/plotOverTime.py
@@ -2,20 +2,23 @@ import time
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
+import scipy.misc
+import glob
+import os
 
 
 f = open('test1.csv')
 csv_f = list(csv.reader(f))
 
 plt.ion()
-
+gifArr = []
 fig, ax = plt.subplots()
 x, y = [float(csv_f[0][1])], [float(csv_f[0][2])]
 xd, yd = [float(csv_f[0][4])], [float(csv_f[0][5])]
 sc = ax.scatter(x, y)
 plt.xlim(0, 80)
-plt.ylim(0, 80)
-plt.title("Nonlinear Pose Estimation (Blue) vs Trajectory(Orange)")
+plt.ylim(-40, 80)
+plt.title("Nonlinear Pose Estimation (Orange) vs Trajectory(Blue)")
 plt.xlabel("x (inches)")
 plt.ylabel("y (inches)")
 plt.draw()
