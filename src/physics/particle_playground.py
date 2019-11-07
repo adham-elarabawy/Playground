d649f9105641628dca4fb981ec1a517b114a1d8c
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-11-07 09:54:09 -0800

    Added history capability

--- src/physics/particle_playground.py
+++ src/physics/particle_playground.py
@@ -5,6 +5,7 @@ import datetime as dt
 import argparse
 
 # constants
+history = True
 k = 9 * 10 ^ 9
 q1 = 1.6 * 1/(10 ^ 19)  # pos
 q2 = q1  # neg
@@ -20,6 +21,9 @@ granularity = 0.01  # increase to have smoother animation. more computation thou
 q1_pos = 0.5
 q2_pos = 1.5
 
+m1 = 9.11 * 1/(10 ^ 31)  # mass of positron
+m2 = 5 * 9.11 * 1/(10 ^ 31)  # mass of electron
+
 q1_vel = 0.5
 q2_vel = 0
 
@@ -32,10 +36,12 @@ distances = []
 plt.ion()
 fig, ax = plt.subplots()
 x, y = [[q1_pos, q2_pos], [0, 0]]
-a1 = ax.arrow(q1_pos, 0, 0, 0, head_width=0.01,
-              head_length=0.05, fc=posColor, ec=posColor)
-a2 = ax.arrow(q2_pos, 0, 0, 0, head_width=0.01,
-              head_length=0.05, fc=negColor, ec=negColor)
+x_hist1, y_hist1 = [[q1_pos], [0]]
+x_hist2, y_hist2 = [[q2_pos], [0]]
+ar1 = ax.arrow(q1_pos, 0, 0, 0, head_width=0.01,
+               head_length=0.05, fc=posColor, ec=posColor)
+ar2 = ax.arrow(q2_pos, 0, 0, 0, head_width=0.01,
+               head_length=0.05, fc=negColor, ec=negColor)
 sc = ax.scatter(x, y, c="black")
 plt.xlim(left_bound_screen, right_bound_screen)
 plt.ylim(-0.5, 2)
@@ -56,39 +62,40 @@ while not collided:
     force = k * abs(q1) * abs(q2) / ((q2_pos - q1_pos) * (q2_pos - q1_pos))
     forces.append(force)
     distances.append(q2_pos-q1_pos)
-    acceleration = force / mass
+    a1 = force / m1
+    a2 = force / m2
     if((q1 > 0) == (q2 > 0)):  # both charges are the same sign
         if(q1_pos < q2_pos):  # if q1 is to the left of q2
-            q1_vel -= acceleration * granularity
+            q1_vel -= a1 * granularity
             q1_pos += q1_vel * granularity
             q1_accel = -1
 
-            q2_vel += acceleration * granularity
+            q2_vel += a2 * granularity
             q2_pos += q2_vel * granularity
             q2_accel = 1
         else:  # if q2 is to the left of q1
-            q2_vel -= acceleration * granularity
+            q2_vel -= a2 * granularity
             q2_pos += q2_vel * granularity
             q2_accel = -1
 
-            q1_vel += acceleration * granularity
+            q1_vel += a1 * granularity
             q1_pos += q1_vel * granularity
             q1_accel = 1
     else:  # both charges are different signs
         if(q1_pos < q2_pos):  # if q1 is to the left of q2
-            q1_vel += acceleration * granularity
+            q1_vel += a1 * granularity
             q1_pos += q1_vel * granularity
             q1_accel = 1
 
-            q2_vel -= acceleration * granularity
+            q2_vel -= a2 * granularity
             q2_pos += q2_vel * granularity
             q2_accel = -1
         else:  # if q2 is to the left of q1
-            q2_vel += acceleration * granularity
+            q2_vel += a2 * granularity
             q2_pos += q2_vel * granularity
             q2_accel = 1
 
-            q1_vel -= acceleration * granularity
+            q1_vel -= a1 * granularity
             q1_pos += q1_vel * granularity
             q1_accel = -1
 
@@ -98,18 +105,28 @@ while not collided:
     time_elapsed = (b_time-a_time).total_seconds()
     x, y = [[q1_pos, q2_pos], [0, 0]]
 
+    x_hist1.append(q1_pos)
+    x_hist2.append(q2_pos)
+    y_hist1.append(0)
+    y_hist2.append(0)
+
     sc.remove()
-    a1.remove()
-    a2.remove()
+    ar1.remove()
+    ar2.remove()
 
-    a1 = ax.arrow(q1_pos, 0, q1_accel*force*arrowAmp, 0, head_width=0.03,
-                  head_length=0.05, fc=posColor, ec=posColor)
-    a2 = ax.arrow(q2_pos, 0, q2_accel*force*arrowAmp, 0, head_width=0.03,
-                  head_length=0.05, fc=negColor, ec=negColor)
+    ar1 = ax.arrow(q1_pos, 0, q1_accel*force*arrowAmp, 0, head_width=0.03,
+                   head_length=0.05, fc=posColor, ec=posColor)
+    ar2 = ax.arrow(q2_pos, 0, q2_accel*force*arrowAmp, 0, head_width=0.03,
+                   head_length=0.05, fc=negColor, ec=negColor)
 
+    if(history):
+        hist1 = plt.plot(x_hist1, y_hist1, color='thistle')
+        hist2 = plt.plot(x_hist2, y_hist2, color='thistle')
     sc = plt.scatter(x, y, color='black')
     timeseries = ax.plot(distances, forces, color='#9467bd')
 
     if(granularity > time_elapsed):
         plt.pause(granularity - time_elapsed)
+    else:
+        print('granularity too fine for computational resources (loop is running slower than the indicated granularity~timestep). \nIf you would like a more accurate simulation, increase granularity at the cost of animation smoothness.')
     fig.canvas.draw_idle()
