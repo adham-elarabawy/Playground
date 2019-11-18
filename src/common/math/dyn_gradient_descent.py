4806155fa7d0c7974ced977a1ec13650e5cbc922
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-11-18 14:03:44 -0800

    Removed proportional steps due to ascent bug

--- src/common/math/dyn_gradient_descent.py
+++ src/common/math/dyn_gradient_descent.py
@@ -1,16 +1,19 @@
 from sympy import *
 import matplotlib.pyplot as plt
 import numpy
+import random
 
 # -- CONFIG -- #
 increment = 0.025
-DESCEND = True
+DESCEND = False
 kP = 0.05
 dt = 0.1
 cutoff = 0.001
-upper_bound = 4  # purely visual, doesn't affect algorithm
-lower_bound = -upper_bound
-starting_point = -1  # keep within upper and lower bounds
+upper_x = 4  # purely visual, doesn't affect algorithm
+lower_x = -upper_x
+upper_y = 10
+lower_y = -upper_y
+starting_point = random.uniform(-2, 2)  # keep within upper and lower bounds
 
 x, y = symbols('x y')
 expr = 5*(x**3) - (x**7)
@@ -19,11 +22,11 @@ expr_deriv = expr.diff(x)
 print(f'Descending? {DESCEND}\nEquation: {expr}\nDerivative: {expr_deriv}')
 
 # original function
-o_x = numpy.linspace(lower_bound, upper_bound, 300)
+o_x = numpy.linspace(lower_x, upper_x, 300)
 o_y = [expr.subs(x, val) for val in o_x]
 
 # derivative function
-d_x = numpy.linspace(lower_bound, upper_bound, 300)
+d_x = numpy.linspace(lower_x, upper_x, 300)
 d_y = [expr_deriv.subs(x, val) for val in o_x]
 
 curr_x = starting_point
@@ -34,8 +37,8 @@ fig, ax = plt.subplots()
 sc = ax.plot(d_x, d_y, c="grey")
 sc = ax.plot(o_x, o_y, c="black")
 scp = ax.scatter(curr_x, curr_y, color='#9467bd')
-plt.xlim(lower_bound, upper_bound)
-plt.ylim(-10, 10)
+plt.xlim(lower_x, upper_x)
+plt.ylim(lower_y, upper_y)
 plt.title("Gradient Descent Visualization")
 plt.xlabel("x")
 plt.ylabel("y")
@@ -65,6 +68,6 @@ while not FINISHED:
         increment = increment/2
         if increment <= cutoff:
             FINISHED = True
-    if dy == 0:
+    if dy == 0 or curr_x > upper_x or curr_x < lower_x or curr_y > upper_y or curr_y < lower_y:
         FINISHED = True
     plt.pause(dt)
